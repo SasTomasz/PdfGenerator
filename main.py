c58caca8fa156e76aa9c676728848ab4b4121d29
tomasz_sas <tomaszsas.dev@gmail.com>
2024-02-01 08:10:03 +0100

    Add a class extending fpdf to add custom footer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from fpdf import FPDF
+
+from pdf_helper import MyPdf
 
 
 def add_footer(name):
@@ -8,15 +9,14 @@ def add_footer(name):
     pdf.cell(0, 5, name, 1, 0, align="R")
 
 
-pdf = FPDF()
-pdf.set_margins(0, 0, 0)
+pdf = MyPdf()
 
 df = pd.read_csv("./topics.csv")
 for index, item in df[['Topic', 'Pages']].iterrows():
     pdf.add_page()
     pdf.set_font('Arial', 'B', 16)
     pdf.cell(0, 10, item.iloc[0], "B")
-    add_footer(item.iloc[0])
+    pdf.set_footer(item.iloc[0])
     for i in range(item.iloc[1] - 1):
         pdf.add_page()
 pdf.output('output.pdf')
---
+++ pdf_helper.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+
+
+class MyPdf(FPDF):
+    def __init__(self):
+        super().__init__()
+        self.page_name = ""
+
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, self.page_name, 0, 0, "R")
+
+    def set_footer(self, page_name):
+        self.page_name = page_name
