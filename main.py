b3b5a9092cf915f40a03b9890369337b46898ac2
tomasz_sas <tomaszsas.dev@gmail.com>
2024-02-01 08:41:52 +0100

    Delete unused func and change fonts

--- main.py
+++ main.py
@@ -2,21 +2,15 @@ import pandas as pd
 
 from pdf_helper import MyPdf
 
-
-def add_footer(name):
-    pdf.set_y(-25)
-    pdf.set_font('Arial', 'I', 8)
-    pdf.cell(0, 5, name, 1, 0, align="R")
-
-
 pdf = MyPdf()
 
 df = pd.read_csv("./topics.csv")
-for index, item in df[['Topic', 'Pages']].iterrows():
+for index, row in df.iterrows():
     pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(0, 10, item.iloc[0], "B")
-    pdf.set_footer(item.iloc[0])
-    for i in range(item.iloc[1] - 1):
+    pdf.set_font('Times', 'B', 22)
+    pdf.cell(0, 10, row["Topic"], "B")
+    pdf.set_footer(row["Topic"])
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
 pdf.output('output.pdf')
+
