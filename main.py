6e08b04a7ffb6d3b613443ede41fc53eba0c6db2
tomasz_sas <tomaszsas.dev@gmail.com>
2024-02-02 09:38:30 +0100

    Add horizontal lines to pages

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 from pdf_helper import MyPdf
 
+
+def add_line():
+    for _ in range(25):
+        pdf.ln()
+        pdf.set_font('Times', 'B', 22)
+        pdf.cell(0, 10, border="B")
+
+
 pdf = MyPdf()
 
 df = pd.read_csv("./topics.csv")
@@ -9,8 +17,10 @@ for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font('Times', 'B', 22)
     pdf.cell(0, 10, row["Topic"], "B")
+    add_line()
     pdf.set_footer(row["Topic"])
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        add_line()
 pdf.output('output.pdf')
 
